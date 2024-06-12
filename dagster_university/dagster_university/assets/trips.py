ff177ee9e17c00cc7b91d4aea7a544533406ddd3
SCGlass <sam.charles.glass@gmail.com>
2024-06-12 21:33:38 +0200

    lesson 7 started

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,6 +1,8 @@
 import requests
+from dagster_duckdb import DuckDBResource
 from . import constants
 from dagster import asset
+
 
 @asset
 def taxi_trips_file() -> None:
@@ -28,3 +30,47 @@ def taxi_zones_file() -> None:
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_taxi_zones.content)
 
+@asset(
+    deps=["taxi_trips_file"]
+)
+def taxi_trips(database: DuckDBResource) -> None:
+    """
+      The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    sql_query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(sql_query)
+
+@asset(
+    deps=[taxi_zones_file]
+)
+def taxi_zones(database: DuckDBResource) -> None:
+    query = f"""
+        create or replace table zones as (
+            select
+                LocationID as zone_id,
+                zone,
+                borough,
+                the_geom as geometry
+            from '{constants.TAXI_ZONES_FILE_PATH}' 
+        );
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(query)
